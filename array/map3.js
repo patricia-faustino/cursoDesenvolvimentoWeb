e73e2d39024c5a97510172db28cc223c5394a889
Patricia <patriciafaustinobr@gmail.com>
2020-10-08 13:22:26 -0300

    Criando o proprio map

---
+++ array/map3.js
@@ -0,0 +1,18 @@
+Array.prototype.map2 = function(callback){
+    const newArray = []
+    for(let i; i < this.length; i++){
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+}
+
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lápis", "preco": 41.42 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'
+]
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
