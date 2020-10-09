d6c2ee02bf992063d4d125b86024cdd61ee018a9
Patricia <patriciafaustinobr@gmail.com>
2020-10-09 18:31:28 -0300

    Implementacao do metodo concat

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Fernada', 'Jéssica']
+const filhos = ['Reinaldo', 'Fernando']
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([4, 5, 6], 7, [[8, 9]]))
