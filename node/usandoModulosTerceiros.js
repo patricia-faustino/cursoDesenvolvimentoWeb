f43d4ac4bdb7d3cb97600a81a48702daf9236150
Patricia <patriciafaustinobr@gmail.com>
2020-10-12 19:18:47 -0300

    Formas de como utilizar o require

--- node/usandoModulosTerceiros.js
+++ node/usandoModulosTerceiros.js
@@ -1,2 +1,2 @@
 const _ = require('lodash') // procura o import no index
-setInterval(() => console.log(_.random(500, 0)), 2000) // executar funcao de tempos em tempos
+setInterval(() => console.log(_.random(500, 0), 2000)) // executar funcao de tempos em tempos
